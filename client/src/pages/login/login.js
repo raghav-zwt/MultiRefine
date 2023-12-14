41c9e70586e56c8da7c7574f95d059b7c33fc5c5
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 14:51:17 +0530

    design

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "../../layouts/layout.js"
-import axios from "axios";
 
 const Login = () => {
   const [urlCode, setUrlCode] = useState("")
@@ -10,29 +9,9 @@ const Login = () => {
     const code = urlSearchParams.get('code');
     if (code) {
       setUrlCode(code)
-      webflowAccessToken(code);
     }
     // eslint-disable-next-line
   }, []);
-
-  const webflowAccessToken = async (code) => {
-    const URLData = new FormData();
-    const url = code; 
-    URLData.append("code", url);
-
-    try {
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_API_URL}/auth/getAccessToken`,
-        URLData
-      );
-
-      if (data.success) {
-        console.log(data)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <>
--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,4 @@
 import { dotenvFile } from "../helper/dotenv.js"
-import axios from "axios";
 
 dotenvFile;
 
@@ -16,93 +15,5 @@ const getAccessToken = async (req, res) => {
         throw error;
     }
 };
-
-const getWebFlowAccessToken = async (wcode) => {
-    try {
-        const clientId = process.env.WEBFLOW_CLIENT_KEY;
-        const clientSecret = process.env.WEBFLOW_SECRET_KEY;
-        const redirectUri = process.env.WEBFLOW_REDIRECT_URI;
-        const authorizationCode = wcode;
-
-        const data = {
-            grant_type: 'authorization_code',
-            client_id: clientId,
-            client_secret: clientSecret,
-            redirect_uri: redirectUri,
-            code: authorizationCode,
-        }
-
-        console.log(data);
-
-        const response = await axios.post('https://api.webflow.com/oauth/access_token', data);
-        const accessToken = response.data.access_token;
-
-        if (accessToken) {
-            authorizedByUser(accessToken);
-        }
-
-        res.json({ message: 'Success', accessToken });
-    } catch (error) {
-        console.error('Error getting access token:', error.message);
-        throw error;
-    }
-};
-
-const authorizedByUser = async (bearerTokenAccess) => {
-    try {
-        const url = "https://api.webflow.com/v2/token/authorized_by";
-        const bearer_token_access = "YOUR_BEARER_TOKEN"; // Replace with your actual bearer token
-
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": `Bearer ${bearer_token_access}`,
-            },
-        });
-
-        if (response.ok) {
-            const result = await response.json();
-
-            console.log(
-                result.email,
-                bearer_token_access
-            );
-
-            const Password = generatePassword(result.email, bearer_token_access);
-
-            const data = (
-                result.id,
-                result.email,
-                result.firstName,
-                result.lastName,
-                Password
-            );
-
-            console.log(data);
-
-            const yourDate = new Date();
-            const newDate = yourDate.toISOString().split("T")[0];
-
-            const data2 = (result.id, newDate, bearer_token_access);
-
-            console.log(data2);
-        } else {
-            console.error(`Error! status: ${response.status}`);
-        }
-    } catch (err) {
-        console.error(err.message || err);
-    }
-};
-
-function generatePassword(email, access_token) {
-    let pass = "";
-    let str = email + access_token;
-    for (let i = 1; i <= 8; i++) {
-        let char = Math.floor(Math.random() * str.length + 1);
-        pass += str.charAt(char);
-    }
-    return pass;
-}
 
 export { getAccessToken }
