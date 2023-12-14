024468aa3702d975c8b020411c4ab8a6c081a77f
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 16:55:21 +0530

    up

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,9 +3,6 @@ import Layout from "../layouts/layout.js"
 
 const index = () => {
 
-  const redirectUri = 'https://multirefine-api.onrender.com/login/auth';
-  window.location.href = redirectUri;
-
   return (
     <>
       <Layout>
