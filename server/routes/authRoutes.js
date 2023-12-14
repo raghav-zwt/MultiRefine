850429326e7cbb6b62e32ecaa5a292d7e0c536cc
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 13:17:49 +0530

    post

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getAccessToken } from "../controllers/authControllers.js";
 const router = express.Router();
 
-router.get("/getAccessToken", getAccessToken);
+router.post("/getAccessToken", getAccessToken);
 
 export default router;
