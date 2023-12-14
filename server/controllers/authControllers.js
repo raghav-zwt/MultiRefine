3f68122556590efe5c8a493c7c42a3494d62f1b1
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 16:47:16 +0530

    new

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -5,7 +5,7 @@ dotenvFile;
 
 const auth = async (req, res) => {
     try {
-        const webflowAuthUrl = `https://webflow.com/oauth/authorize?response_type=code&client_id=${process.env.WEBFLOW_CLIENT_KEY}&redirect_uri=${process.env.WEBFLOW_REDIRECT_URI}`;
+        const webflowAuthUrl = `https://webflow.com/oauth/authorize?client_id=".${process.env.WEBFLOW_CLIENT_KEY}."&response_type=code&state=".${process.env.WEBFLOW_SECRET_KEY}`;
         res.redirect(webflowAuthUrl);
     } catch (error) {
         console.error('Error getting access token:', error.message);
