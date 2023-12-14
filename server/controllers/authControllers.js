579ef3a29a470ab8e6374e603142da57f8cbb9b7
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 13:15:17 +0530

    err

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -8,7 +8,8 @@ const getAccessToken = async (req, res) => {
         const { code } = req.body;
         console.log(code);
         const accessToken = await getWebFlowAccessToken(code);
-        
+
+        console.log(accessToken);
         return accessToken;
     } catch (error) {
         console.error('Error getting access token:', error.message);
@@ -17,29 +18,34 @@ const getAccessToken = async (req, res) => {
 };
 
 const getWebFlowAccessToken = async (wcode) => {
-    const clientId = process.env.WEBFLOW_CLIENT_KEY;
-    const clientSecret = process.env.WEBFLOW_SECRET_KEY;
-    const redirectUri = process.env.WEBFLOW_REDIRECT_URI;
-    const authorizationCode = wcode;
+    try {
+        const clientId = process.env.WEBFLOW_CLIENT_KEY;
+        const clientSecret = process.env.WEBFLOW_SECRET_KEY;
+        const redirectUri = process.env.WEBFLOW_REDIRECT_URI;
+        const authorizationCode = wcode;
 
-    const data = {
-        grant_type: 'authorization_code',
-        client_id: clientId,
-        client_secret: clientSecret,
-        redirect_uri: redirectUri,
-        code: authorizationCode,
+        const data = {
+            grant_type: 'authorization_code',
+            client_id: clientId,
+            client_secret: clientSecret,
+            redirect_uri: redirectUri,
+            code: authorizationCode,
+        }
+
+        console.log(data);
+
+        const response = await axios.post('https://api.webflow.com/oauth/access_token', data);
+        const accessToken = response.data.access_token;
+
+        if (accessToken) {
+            authorizedByUser(accessToken);
+        }
+
+        res.json({ message: 'Success', accessToken });
+    } catch (error) {
+        console.error('Error getting access token:', error.message);
+        throw error;
     }
-
-    console.log(data);
-
-    const response = await axios.post('https://api.webflow.com/oauth/access_token', data);
-    const accessToken = response.data.access_token;
-
-    if (accessToken) {
-        authorizedByUser(accessToken);
-    }
-
-    res.json({ message: 'Success', accessToken });
 };
 
 const authorizedByUser = async (bearerTokenAccess) => {
