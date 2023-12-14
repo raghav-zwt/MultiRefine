0073cf224bab2e3f4b47fa9026cfa53cb3fad851
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-14 14:23:04 +0530

    headers

--- server/app.js
+++ server/app.js
@@ -11,13 +11,6 @@ const app = express();
 
 dotenvFile;
 
-app.use((req,res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.setHeader('Access-Control-Allow-Headers',"*");
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
