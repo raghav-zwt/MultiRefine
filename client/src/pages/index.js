4f3dbfba78257dc224ae38080166a5e3646e3230
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-15 12:24:51 +0530

    das

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,28 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Layout from "../layouts/layout.js"
 
 const HomePage = () => {
-
-  useEffect(() => {
-    const redirectToAuthorization = async () => {
-      try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/webflow-auth/authorize`);
-
-        if (!response.ok) {
-          throw new Error(`Failed to fetch authorization URL. Status: ${response.status}`);
-        }
-
-        const { authorizationUrl } = await response.json();
-        window.location.href = authorizationUrl;
-      } catch (error) {
-        console.error('Error fetching authorization URL:', error);
-      }
-    };
-
-    // Call the function to initiate the redirection
-    redirectToAuthorization();
-  }, []); // Empty dependency array ensures that this effect runs only once on mount
-
   return (
     <>
       <Layout>
