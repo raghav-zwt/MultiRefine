e50f1cdf85ad9d1809e43c2f6b50f732ae3a7f7f
Jenil-Zealousweb <jenil@zealousweb.com>
2023-12-15 12:03:35 +0530

    d

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Layout from "../layouts/layout.js"
 
 const index = () => {
 
-  useEffect(() => {
-    const redirectToAuthorization = async () => {
-      try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/authorize`);
-        const { authorizationUrl } = await response.json();
-
-        window.location.href = authorizationUrl;
-      } catch (error) {
-        console.error('Error fetching authorization URL:', error);
-      }
-    };
-
-    redirectToAuthorization();
-  }, []);
+  const response = fetch(`${process.env.REACT_APP_API_URL}/authorize`);
+  window.location.href = response;
 
   return (
     <>
