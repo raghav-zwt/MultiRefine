375c333abffbcccec8b96229dc64191c03bea25e
Jenil-Zealousweb <jenil@zealousweb.com>
2024-01-17 16:15:47 +0530

    fsd

--- server/app.js
+++ server/app.js
@@ -14,7 +14,13 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 dotenvFile;
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://multi-refine.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 app.use(cors({ credentials: true, origin: ["*"] }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
