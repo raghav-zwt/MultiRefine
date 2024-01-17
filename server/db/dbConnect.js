8a2d3a728b65e4016b528393086707aff0373e29
Jenil-Zealousweb <jenil@zealousweb.com>
2024-01-17 19:24:33 +0530

    dasdas

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -14,7 +14,6 @@ const dbConnect = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10,
