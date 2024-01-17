8443fbc5811e1e27719fdd8891185450eb74aa45
Jenil-Zealousweb <jenil@zealousweb.com>
2024-01-17 16:35:28 +0530

    das

--- server/app.js
+++ server/app.js
@@ -22,11 +22,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(morgan("dev"));
