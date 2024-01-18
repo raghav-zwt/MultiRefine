b0192647be3c76c57a0e3be15b3ea767c7eb1e03
Jenil-Zealousweb <jenil@zealousweb.com>
2024-01-18 20:04:42 +0530

    dasda

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,14 +1,12 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 
-// Load environment variables
 const dotenvResult = dotenv.config({ path: '.env' });
 if (dotenvResult.error) {
     console.error("Error loading .env file:", dotenvResult.error);
     throw dotenvResult.error;
 }
 
-// Create a connection pool
 const dbConnect = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -25,24 +23,7 @@ const dbConnect = mysql.createPool({
     keepAliveInitialDelay: 0,
 });
 
-// Handle connection pool events
-dbConnect.on("acquire", (connection) => {
-    console.log("Connection %d acquired", connection.threadId);
-});
 
-dbConnect.on("connection", (connection) => {
-    console.log("New database connection created");
-});
-
-dbConnect.on("enqueue", () => {
-    console.log("Waiting for available connection slot");
-});
-
-dbConnect.on("release", (connection) => {
-    console.log("Connection %d released", connection.threadId);
-});
-
-// Handle errors in the connection pool
 dbConnect.on("error", (err) => {
     console.error("Database pool error:", err);
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
@@ -53,19 +34,16 @@ dbConnect.on("error", (err) => {
     }
 });
 
-// Connect to the database
 async function handleDisconnect() {
     try {
         const connection = await getConnection(dbConnect);
         console.log("DB connection successful");
-        // Perform your database operations here
         connection.release();
     } catch (error) {
         console.error("DB connection failed:", error.message);
     }
 }
 
-// Function to get a connection from the pool
 function getConnection(pool) {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
@@ -78,7 +56,6 @@ function getConnection(pool) {
     });
 }
 
-// Call the handleDisconnect function
 handleDisconnect();
 
 export { dbConnect };
