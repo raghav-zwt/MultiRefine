7ac1a5584335f4e8fd20b4f5285c876b7bc2d761
Jenil-Zealousweb <jenil@zealousweb.com>
2024-01-22 20:21:25 +0530

    loader

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <>
       <div className='Loader-style'>
-        <img src={LoaderSpin} width={80} height={80} alt="loading..." />
+        <img src={LoaderSpin} width={50} height={50} alt="loading..." />
       </div>
     </>
   )
